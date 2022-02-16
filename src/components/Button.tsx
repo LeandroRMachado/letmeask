b2cf964e530b158ce9484b0fc47d7ca1b9ce3213
LeandroRMachado <leandroskullofwar@outlook.com>
2022-02-15 23:12:39 -0300

    Create hook 'useRoom', page Adm, function 'like'

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,16 @@ import { ButtonHTMLAttributes } from "react"
 
 import "../styles/button.scss"
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
 
-export function Button(props: ButtonProps) {
+export function Button({isOutlined = false, ...props }: ButtonProps) {
   // let = let it change 
   return (
-    <button className="button" {...props}/>
+    <button className={`button ${isOutlined ? 'outlined' : ''} `} {...props}/>
+    //condicional - caso isOutlined exista, eu vou por uma className a mais chamada "outlined", se não eu não vou por className a mais nenhuma
   )
 }
+
+//   ...   rest operator, ou seja eu estou pegando o resto - tudo que não for "isOutlined" eu estou jogando dentro de "...props"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,6 +36,11 @@ export function Home() {
         return
       }
 
+      if ((await roomRef).val().endedAt) {
+        alert('Room already closed')
+        return
+      }
+
       history.push(`/rooms/${roomCode}`)
     }
   
